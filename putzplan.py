d8d7c2e03e55a9f8523196fa69fa42a7df8e1c44
Patrick Bollmann <39247927+patrickbollmann@users.noreply.github.com>
2019-07-21 18:11:50 +0200

    Add files via upload

--- putzplan.py
+++ putzplan.py
@@ -2,9 +2,27 @@ import pymysql
 from operator import itemgetter
 count = 0
 user = []
+done = []
+penalty = []
 schedule = [0,0,0,0,0,0,0]
-wert = [8,7,7,6,5,5,5] #Kosten in folgender Reihenfolge: Wohnzimmer,Küche,Treppenhaus,Bad,Toiletten,Müll,Keller  (durchschnitt 6)
+wert = [8,8,6,6,6,5,4] #Kosten in folgender Reihenfolge: Wohnzimmer,Küche,Treppenhaus,Bad,Toiletten,Müll,Keller
 sql = ""
+
+def bereichid(x):
+    if x == "bad":
+        return 0
+    if x == "kueche":
+        return 1
+    if x == "muell":
+        return 2
+    if x == "toiletten":
+        return 3
+    if x == "wohnzimmer":
+        return 4
+    if x == "treppenhaus":
+        return 5
+    if x == "keller":
+        return 6
 
 conn = pymysql.connect(host="db.danielki.de",user="user",
                                         passwd="pass",
@@ -12,11 +30,38 @@ conn = pymysql.connect(host="db.danielki.de",user="user",
 										use_unicode=True,
 										charset="utf8")
 cur = conn.cursor()
-cur.execute("SELECT * FROM `person` WHERE active = 1 ORDER BY score") #wähle aktive Nutzer, sortiere diese aufsteigend nach score
 
+#penaltys vergeben
+cur.execute("SELECT * FROM `done` WHERE scheduleid = (SELECT MAX(scheduleid) FROM done)")
+for row in cur:
+    print(row)
+    if row[1] != 1:
+        done.append("bad")
+    if row[2] != 1:
+        done.append("kueche")
+    if row[3] != 1:
+        done.append("muell")
+    if row[4] != 1:
+        done.append("toiletten")
+    if row[5] != 1:
+        done.append("wohnzimmer")
+    if row[6] != 1:
+        done.append("treppenhaus")
+    if row[7] != 1:
+        done.append("keller")
+print(done)
+cur.execute("SELECT bad,kueche,muell,toiletten,wohnzimmer,treppenhaus,keller FROM `schedule` WHERE scheduleid = (SELECT MAX(scheduleid) FROM schedule)")
+for row in cur:
+    for i in done:
+        penalty.append(row[bereichid(i)])
+
+print(penalty)
+for i in penalty:
+    cur.execute("UPDATE person SET score = score -10 WHERE personid =" + str(i))
+
+#Plan erstellen
+cur.execute("SELECT * FROM `person` WHERE active = 1 ORDER BY score")  #wähle aktive Nutzer, sortiere diese aufsteigend nach score
 print(cur.description)
-print()
-
 for row in cur:
     user.append([row[0],row[2]]) # erstelle liste mit ID und Sore der Nutzer. Sortiert nach score aufsteigend
 
