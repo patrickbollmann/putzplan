46a8ebaae89b97d1a9d5a43f3258f19c102ff142
Patrick Bollmann <39247927+patrickbollmann@users.noreply.github.com>
2019-11-10 21:10:24 +0100

    Updated putzplan.py
    updated to stable version

--- putzplan.py
+++ putzplan.py
@@ -2,6 +2,7 @@ import pymysql
 from operator import itemgetter
 count = 0
 user = []
+inactive = []
 done = []
 penalty = []
 schedule = [0,0,0,0,0,0,0]
@@ -24,8 +25,8 @@ def bereichid(x):
     if x == "keller":
         return 6
 
-conn = pymysql.connect(host="db.danielki.de",user="user",
-                                        passwd="pass",
+conn = pymysql.connect(host="db.danielki.de",user="patrick",
+                                        passwd="VaX(u248!)}pDK?F",
                                         db="putzplan",
 										use_unicode=True,
 										charset="utf8")
@@ -54,17 +55,31 @@ cur.execute("SELECT bad,kueche,muell,toiletten,wohnzimmer,treppenhaus,keller FRO
 for row in cur:
     for i in done:
         penalty.append(row[bereichid(i)])
-
+print("penaltys gehen an:")
 print(penalty)
 for i in penalty:
     cur.execute("UPDATE person SET score = score -10 WHERE personid =" + str(i))
+for i in penalty:
+    cur.execute("UPDATE person SET penalty = penalty +1 WHERE personid =" + str(i))
+
+#inaktive nutzer bekommen score +10
+cur.execute("SELECT personid FROM `person` WHERE active = 0")  #wähle inaktive nutzer
+print("inaktiv sind:")
+print(cur.description)
+for row in cur:
+    inactive.append(row[0])
+
+for i in inactive:
+    cur.execute("UPDATE person SET score = score +10 WHERE personid =" + str(i))
+    conn.commit()
+
 
 #Plan erstellen
 cur.execute("SELECT * FROM `person` WHERE active = 1 ORDER BY score")  #wähle aktive Nutzer, sortiere diese aufsteigend nach score
 print(cur.description)
 for row in cur:
     user.append([row[0],row[2]]) # erstelle liste mit ID und Sore der Nutzer. Sortiert nach score aufsteigend
-
+print("aktive user:")
 print(user)
 
 for i in range(len(schedule)):  #verteilt Aufgaben nach Score
@@ -74,9 +89,10 @@ for i in range(len(schedule)):  #verteilt Aufgaben nach Score
 
 user = sorted(user, key=itemgetter(0))
 for i in range(len(user)):  #update neue scores in db
-    cur.execute("UPDATE person SET score = "+ str(user[i][1]) + " WHERE personid = " + str(i+1))
+    cur.execute("UPDATE person SET score = "+ str(user[i][1]) + " WHERE personid = " + str(user[i][0]))
 conn.commit()
 
+print("neue sql, user, plan:")
 print(sql)
 print(user)
 print(schedule)
@@ -97,6 +113,16 @@ for row in cur:
 #schreibe neuen eintrag in done
 cur.execute("INSERT INTO `done` (`scheduleid`, `bad`, `kueche`, `muell`, `toiletten`, `wohnzimmer`, `treppenhaus`, `keller`) VALUES ('"+ str(scheduleid) +"', '0', '0', '0', '0', '0', '0', '0');")
 conn.commit()
+
+#setze beschwerden Anmerkungen zurück
+cur.execute("UPDATE beschwerde SET bad ='', kueche = '', keller = '', muell = '', toiletten = '', treppenhaus = '', wohnzimmer = '' WHERE id = 1")
+conn.commit()
+
+#setze alle nutzer auf active
+for i in inactive:
+    cur.execute("UPDATE person SET active = 1 WHERE personid =" + str(i))
+    conn.commit()
+
 
 cur.close()
 conn.close()
