424c272fe9e633e5ee8d9bc733a5f48ed9cf3f98
Patrick Bollmann <pbollman@mail.upb.de>
2021-11-15 13:15:22 +0100

    add user roles

--- putzplan.py
+++ putzplan.py
@@ -10,10 +10,27 @@ print("Strafen gehen an:")
 
 for row in strafen:
     print(row["User"])
-    db.query("UPDATE person SET score = score -"+str((row['Wert']+5))+", penalty = penalty+1 WHERE name ='" + row['User']+"'")
+    db.query("UPDATE person SET score = score -"+str((row['Wert']+1))+", penalty = penalty+1 WHERE name ='" + row['User']+"'")
    
+#Plan special groups
+groups = db.query("SELECT Gruppe FROM person GROUP BY Gruppe")
+for group in groups:
+    print( "group: "+str(group["Gruppe"]))
+    grouplocations = db.query("SELECT Name, Wert FROM `location` WHERE Gruppe = "+str(group["Gruppe"])+" ORDER BY Wert DESC")
+    for row in grouplocations:
+        location = row["Name"]
+        wert = row["Wert"]
+        user = db.query("SELECT name FROM `person` WHERE active = 1 AND Gruppe = "+str(group["Gruppe"])+" ORDER BY score ASC LIMIT 1")  #Wähle user mit geringstem Score
+        for x in user:
+            username = x["name"]
+            db.query("UPDATE person SET score = score + "+str(wert)+" WHERE name ='"+username+"'") #setzte neuen Score für user
+            db.query("UPDATE location SET user = '"+username+"', Beschwerde = '', Done = 0 WHERE Name = '"+location+"'") #vergebe Aufgabe, lösche Beschwerde, setze Done auf 0
+            print(location+": "+username)
+
+
 #Plan erstellen
-locationquery = db.query("SELECT Name, Wert FROM `location` ORDER BY Wert DESC")
+locationquery = db.query("SELECT Name, Wert FROM `location` WHERE Gruppe IS NULL ORDER BY Wert DESC")
+print( "non group specific tasks")
 for row in locationquery:
     location = row["Name"]
     wert = row["Wert"]
@@ -29,4 +46,4 @@ inactive = db.query("SELECT personid, name FROM `person` WHERE active < 1")  #w�
 print("inaktiv sind:")
 for row in inactive:
     print(row["name"])
-    db.query("UPDATE person SET score = score +10, active = active +1 WHERE personid =" + str(row["personid"]))
+    db.query("UPDATE person SET score = score +2, active = active +1 WHERE personid =" + str(row["personid"]))
--- putzplan.sql
+++ putzplan.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.6deb5
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Erstellungszeit: 26. Jan 2020 um 15:35
--- Server-Version: 5.7.28-0ubuntu0.18.04.4
--- PHP-Version: 7.2.26-1+ubuntu18.04.1+deb.sury.org+1
+-- Host: 192.168.0.26:3306
+-- Erstellungszeit: 15. Nov 2021 um 12:13
+-- Server-Version: 8.0.25
+-- PHP-Version: 7.4.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -27,26 +28,14 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `location` (
-  `ID` int(11) NOT NULL,
+  `ID` int NOT NULL,
   `Name` text NOT NULL,
-  `Wert` int(11) NOT NULL,
+  `Wert` int NOT NULL,
   `User` varchar(20) NOT NULL,
-  `Done` int(11) NOT NULL,
-  `Beschwerde` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Daten für Tabelle `location`
---
-
-INSERT INTO `location` (`ID`, `Name`, `Wert`, `User`, `Done`, `Beschwerde`) VALUES
-(1, 'Wohnzimmer', 8, 'Patrick', 1, ''),
-(2, 'Kueche', 8, 'Laura', 1, ''),
-(3, 'Treppenhaus', 6, 'Patrick', 1, ''),
-(4, 'Geschirrtuecher', 5, 'Patrick', 1, ''),
-(5, 'Toiletten', 6, 'Mayra', 1, ''),
-(6, 'Muell', 5, 'Maike', 1, ''),
-(7, 'Bad', 6, 'Philipp', 0, '');
+  `Done` int NOT NULL,
+  `Beschwerde` text NOT NULL,
+  `Gruppe` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 -- --------------------------------------------------------
 
@@ -55,23 +44,13 @@ INSERT INTO `location` (`ID`, `Name`, `Wert`, `User`, `Done`, `Beschwerde`) VALU
 --
 
 CREATE TABLE `person` (
-  `personid` int(8) NOT NULL,
+  `personid` int NOT NULL,
   `name` text NOT NULL,
-  `score` int(11) NOT NULL,
-  `penalty` int(11) NOT NULL,
-  `active` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Daten für Tabelle `person`
---
-
-INSERT INTO `person` (`personid`, `name`, `score`, `penalty`, `active`) VALUES
-(1, 'Patrick', 287, 2, 1),
-(2, 'Philipp', 274, 4, 1),
-(3, 'Mayra', 276, 1, 1),
-(4, 'Maike', 275, 1, 1),
-(5, 'Laura', 274, 1, 1);
+  `score` int NOT NULL,
+  `penalty` int NOT NULL,
+  `active` tinyint(1) NOT NULL,
+  `Gruppe` int DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Indizes der exportierten Tabellen
@@ -97,7 +76,15 @@ ALTER TABLE `person`
 -- AUTO_INCREMENT für Tabelle `location`
 --
 ALTER TABLE `location`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `ID` int NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT für Tabelle `person`
+--
+ALTER TABLE `person`
+  MODIFY `personid` int NOT NULL AUTO_INCREMENT;
+COMMIT;
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
